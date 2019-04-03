74aa2126e9d4eba7fc86bdabbba5a9cd8d24004a
Ray <rezelshome@gmail.com>
2019-04-03 15:55:55 +0800

    Update webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    "babel-polyfill",
-    "./test.js"
-  ],
+  entry: './test.js',
   output: {
-    path: __dirname + '/output/',
-    publicPath: "/output/",
-    filename: 'index.js'
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
       }
     ]
   }
-};
+}
