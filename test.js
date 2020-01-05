2f55279d29395cadb35e1d7c6fdfb4d93300e80c
Ray <rezelshome@gmail.com>
2020-01-05 16:42:31 +0800

    Update dist/bundle

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import { driver } from './src/driver'
-import { LOGIC_R } from './src/constant'
+import { MAP } from './src/constant'
 import { rand, isNull } from './src/util'
 
 
@@ -56,7 +56,7 @@ const init = () => {
     ]),
     c(),
   ])
-  root.struct = LOGIC_R
+  root.struct = MAP
   render()
 }
 
